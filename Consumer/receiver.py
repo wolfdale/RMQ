260a86de94cea5388543b21f57d8ff7769c78be4
Prakhar G Wolf <mywolfdale@gmail.com>
2017-05-29 21:19:13 +0530

    Adder

--- Consumer/receiver.py
+++ Consumer/receiver.py
@@ -2,11 +2,14 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
+#Define a Name for Rabbit Que
 channel.queue_declare(queue='runner')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    param = body.split("+")
+    result = int(param[0]) + int(param[1])
+    print result
 
 channel.basic_consume(callback,
                       queue='runner',
--- Producer/sender.py
+++ Producer/sender.py
@@ -3,9 +3,12 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='runner')
+
+
+
 channel.basic_publish(exchange='',
 			routing_key='runner',
-			body="This msg is sent from sender")
+			body="5+2")
 print "[x] Sent !!"
 connection.close()
 
