bdc14b7d17c4e145df09cb67ac1723dd5684fbac
Prakhar G Wolf <mywolfdale@gmail.com>
2017-05-30 21:27:10 +0530

    SYS ARGS

--- Producer/sender.py
+++ Producer/sender.py
@@ -1,14 +1,14 @@
 import pika
-
+import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='runner')
 
 
-
+msg = sys.argv[1]
 channel.basic_publish(exchange='',
 			routing_key='runner',
-			body="5+2")
+			body=msg)
 print "[x] Sent !!"
 connection.close()
 
